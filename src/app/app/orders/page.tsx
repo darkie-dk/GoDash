06ba44f2afc23fd6ff1e010ebf6b3682c91da4b4
darkie-dk <kevendaniel.kd@gmail.com>
2025-02-28 22:40:35 -0300

    pagination

--- src/app/app/orders/page.tsx
+++ src/app/app/orders/page.tsx
@@ -1,4 +1,3 @@
-import { Input } from '@/components/ui/input'
 import {
   Table,
   TableBody,
@@ -7,40 +6,42 @@ import {
   TableRow,
 } from '@/components/ui/table'
 import { OrderTableRow } from './(components)/order-table-row'
+import { OrderTablefilters } from './(components)/order-table-filters'
+import { Pagination } from '@/components/Pagination'
 
 export default function Orders() {
   return (
     <>
-      <div className="flex-col gap-4">
+      <div className="flex flex-col gap-4">
         <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
-      </div>
-      <div className="space-y-2.5">
-        <form className="flex items-center gap-2">
-          <span className="text-sm font-semibold">Filtros</span>
-          <Input placeholder="Nome do cliente..." className="h-8 w-80" />
-        </form>
 
-        <div className="rounded-md border">
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHead className="w-14"></TableHead>
-                <TableHead className="w-28">Indentificador</TableHead>
-                <TableHead className="w-36">Realizado há</TableHead>
-                <TableHead className="w-32">Status</TableHead>
-                <TableHead>Cliente</TableHead>
-                <TableHead className="w-40">Total </TableHead>
-                <TableHead className="w-36">Fonte</TableHead>
-                <TableHead className="w-36"></TableHead>
-                <TableHead className="w-32"></TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {Array.from({ length: 10 }).map((_, i) => {
-                return <OrderTableRow key={i} />
-              })}
-            </TableBody>
-          </Table>
+        <div className="space-y-2.5">
+          <OrderTablefilters />
+
+          <div className="rounded-md border">
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="w-14"></TableHead>
+                  <TableHead className="w-28">Indentificador</TableHead>
+                  <TableHead className="w-36">Realizado há</TableHead>
+                  <TableHead className="w-32">Status</TableHead>
+                  <TableHead>Cliente</TableHead>
+                  <TableHead className="w-40">Total </TableHead>
+                  <TableHead className="w-36">Fonte</TableHead>
+                  <TableHead className="w-36"></TableHead>
+                  <TableHead className="w-32"></TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {Array.from({ length: 10 }).map((_, i) => {
+                  return <OrderTableRow key={i} />
+                })}
+              </TableBody>
+            </Table>
+          </div>
+
+          <Pagination pageIndex={0} totalCount={26} perPage={10} />
         </div>
       </div>
     </>
